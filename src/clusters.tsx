93c656423588563abfb8781c9930390d2c4f56fb
Dmitry Kondratyev <dmitry.kondratyev@cern.ch>
2024-03-28 18:10:26 -0400

    force to use cluster's own dashboard link

--- src/clusters.tsx
+++ src/clusters.tsx
@@ -5,7 +5,8 @@ import {
   CommandToolbarButton
 } from '@jupyterlab/apputils';
 
-import { IChangedArgs, URLExt } from '@jupyterlab/coreutils';
+// import { IChangedArgs, URLExt } from '@jupyterlab/coreutils';
+import { IChangedArgs } from '@jupyterlab/coreutils';
 
 import * as nbformat from '@jupyterlab/nbformat';
 
@@ -74,18 +75,22 @@ export class DaskClusterManager extends Widget {
       if (!cluster) {
         return;
       }
-      const proxyUrl = URLExt.join(this._serverSettings.baseUrl, 'proxy');
-      const proxyPrefix = new URL(proxyUrl).pathname;
-      if (cluster.dashboard_link.indexOf(proxyPrefix) !== -1) {
-        // If the dashboard link is already proxied using
-        // jupyter_server_proxy, don't proxy again. This
-        // can happen if the user has overridden the dashboard
-        // URL to the jupyter_server_proxy URL manually.
-        options.setDashboardUrl(cluster.dashboard_link);
-      } else {
-        // Otherwise, use the internal proxy URL.
-        options.setDashboardUrl(`dask/dashboard/${cluster.id}`);
-      }
+
+      options.setDashboardUrl(cluster.dashboard_link);
+
+      // const proxyUrl = URLExt.join(this._serverSettings.baseUrl, 'proxy');
+      // const proxyPrefix = new URL(proxyUrl).pathname;
+
+      // if (cluster.dashboard_link.indexOf(proxyPrefix) !== -1) {
+      //   // If the dashboard link is already proxied using
+      //   // jupyter_server_proxy, don't proxy again. This
+      //   // can happen if the user has overridden the dashboard
+      //   // URL to the jupyter_server_proxy URL manually.
+      //   options.setDashboardUrl(cluster.dashboard_link);
+      // } else {
+      //   // Otherwise, use the internal proxy URL.
+      //   options.setDashboardUrl(`dask/dashboard/${cluster.id}`);
+      // }
 
       const old = this._activeCluster;
       if (old && old.id === cluster.id) {
