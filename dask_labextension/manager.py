d2e0d5b50912e96817e19e196cb9c7bfd28c0e7e
Dmitry Kondratyev <dmitry.kondratyev@cern.ch>
2024-03-29 16:19:08 -0400

    testing

--- dask_labextension/manager.py
+++ dask_labextension/manager.py
@@ -275,7 +275,6 @@ def make_cluster_model(
         info = cluster.scheduler_info
     except AttributeError:
         info = cluster.scheduler.identity()
-    raise KeyError(info)
 
     try:
         cores = sum(d["nthreads"] for d in info["workers"].values())
@@ -299,6 +298,7 @@ def make_cluster_model(
         kernel_name=kernel_name,
         kernel_display_name=kernel_display_name
     )
+    raise KeyError(model)
     if adaptive:
         model["adapt"] = {"minimum": adaptive.minimum, "maximum": adaptive.maximum}
 
